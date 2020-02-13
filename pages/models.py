6870912271369bc33eaf91f2602f192228e836a9
Essam <essamabuissa@hotmail.com>
2020-02-13 12:54:31 +0200

    Wiki Project Done

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
+#from pages import views
 
 # Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length = 30)
+    content = models.TextField()
+    last_update = models.DateTimeField(auto_now_add = True)
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('page-detail', args=[str(self.id)])
